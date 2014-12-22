263dfcd5b36a643ee84b41d0cba3784ab9abe069
Rajan Gupta <rajanjamu@gmail.com>
2014-12-22 10:38:38 +0530

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :contacts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'gallery' => 'static_pages#gallery'
   get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  # get 'contact' => 'contacts#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
